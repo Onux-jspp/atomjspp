beb66e7d35e7ae939a269f4914023cd5909de780
Rahul <rahul@arkasoftwares.com>
2018-12-12 18:23:35 +0530

    add and commit files..

---
+++ spec/onuxjspp-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import OnuxjsppView from '../lib/onuxjspp-view';
+
+describe('OnuxjsppView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
